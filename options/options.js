c0d344a7fc6d0cbd6dbaff8ea8937c0d2abc4091
Crowdbotics <team@crowdbotics.com>
2021-10-03 23:51:55 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "winter_math_31081",
+  url: "https://winter_math_31081.botics.co",
+  api: "https://winter_math_31081.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
